63a6acac228eea4ccf6c6914b2f4f837c11a22cb
IdahoEv <evan.dorn@lootcrate.com>
2017-09-13 12:24:24 -0700

    refactored attributes to use getters and setters

--- test/SpriteTest.js
+++ test/SpriteTest.js
@@ -2,6 +2,26 @@ import { expect } from 'chai';
 import Sprite from '../src/Sprite';
 
 describe('Sprite', () => {
+  describe('setting attributes', () => {
+    it('sets the position', () => {
+      const sprite = new Sprite();
+      sprite.position = { x: 1.0, y: 2.0 }
+      expect(sprite.position.x).to.eql(1.0);
+      expect(sprite.position.y).to.eql(2.0);
+    });
+    it('sets the velocity', () => {
+      const sprite = new Sprite();
+      sprite.velocity = { x: 1.0, y: 2.0 }
+      expect(sprite.velocity.x).to.eql(1.0);
+      expect(sprite.velocity.y).to.eql(2.0);
+    });
+    it('sets the rotation', () => {
+      const sprite = new Sprite();
+      sprite.rotation = 3.5;
+      expect(sprite.rotation).to.eql(3.5);
+    });
+  });
+
   describe('Instantiation', () => {
     it('should create a new sprite with x and y coordinates that default to 0', () => {
       const sprite = new Sprite();
